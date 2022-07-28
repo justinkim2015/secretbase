07c9c0f3f6d540e9e4a224df34bf4c499246ae11
justinkim2015 <justinkim2015@gmail.com>
2022-07-28 09:57:11 +0900

    Implement strong params on posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(user_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:post).permit(:title, :body)
+  end
+end
